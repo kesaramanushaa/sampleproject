fc9620b8e0c5f26dd2d348afbddd031249b4af18
kesaramanushaa <kesaramanusha3010@gmail.com>
2025-08-02 11:49:40 +0530

    second update

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app=Flask(__name__)
 def index():
     return "Welcome to MRECW"
 
+@app.route('/home')
+def home():
+    return "Home Page"
+
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=10000)
